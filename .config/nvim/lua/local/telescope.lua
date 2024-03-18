22756bb0091310e149f8fd61f38ba1f54ce6d130
MatthiasJReisinger <mail@mreisinger.com>
2024-03-18 08:14:04 +0100

    [nvim] Add telescope shortcut to resume previous search

--- .config/nvim/lua/local/telescope.lua
+++ .config/nvim/lua/local/telescope.lua
@@ -3,6 +3,7 @@ print("call telescope.lua")
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
 vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
 vim.keymap.set("n", "<leader>fm", "<cmd>Telescope keymaps<cr>")
+vim.keymap.set("n", "<leader>fr", "<cmd>Telescope resume<cr>")
 
 require("telescope").setup({
     defaults = {
